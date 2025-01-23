33ddf7ecf076c805f000433b4579ba936772398a
Sajed Hamdan <132995155+SajedHamdan09@users.noreply.github.com>
2025-01-23 19:22:20 +0200

    Delete AidLebanon directory

--- AidLebanon/src/main/java/com/aidlebanon/AidLebanon/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aidlebanon.AidLebanon.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aidlebanon.AidLebanon.Entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
